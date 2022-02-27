dd39ecfdba4326462dc07d20efa198da9c8682ef
marco <ttap5090@gmail.com>
2022-02-27 01:41:34 -0300

    pergunta UR4SA2U9

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ function mostrarCampoBusca(){
         }else{
         example.style.display = "none"
         }}
+
+let listaNoticias = [
+    {
+        titulo:"abc",
+        descricao:"a1b1c1",
+    },
+    {
+        titulo: "cde",
+        descricao: "c1d1e1",
+
+    },
+]
+function renderizarNoticias (){
+    let espaco = document.getElementById("highlightPrincipal")
+    let template = "";
+    for (let index = 0; index < array.length; index++) {
+        const noticias = listaNoticias[index];
+        
+        template += ` <div class="banner1">
+        <img src="Imagens/image-1.png" style="width: 600px; height: 300px;">
+        <h3>${noticias.titulo}</h3>
+        <p>${noticias.descricao}</p>
+    </div>`
+    }
+    espaco.innerHTML = template;
+}

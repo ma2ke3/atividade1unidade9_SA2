c6dc66ef8cfe067c0cf023977d28fbd42725a210
marco <ttap5090@gmail.com>
2022-03-05 07:39:44 -0300

    Atualizacao AO1 SA2 U9

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//o problema do menu sumir persiste
 let menu = document.getElementById("menu");
 function mostrarMenu(){
     if(menu.style.display !="flex"){
@@ -6,17 +5,6 @@ function mostrarMenu(){
     }else{
     menu.style.display = "none"
     }}
-
-//tentativa de codar a <div class="botao-search"> como toggle de campo de buscar
-//quando apliquei essa máscara de CSS não consegui resolver graficamente: https://www.w3schools.com/howto/howto_css_search_button.asp
-let example = document.getElementById("example");
-function mostrarCampoBusca(){
-        if(example.style.display !="flex"){
-        example.style.display = "flex"
-        }else{
-        example.style.display = "none"
-        }}
-
 let listaNoticias = [
     {
         titulo:"abc",
